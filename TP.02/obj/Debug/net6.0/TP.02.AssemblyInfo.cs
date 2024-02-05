5045be42100cd68660b5cf944939dc5fb681f95a
mathis-de-sousa-pro <mathis.dsa39@gmail.com>
2024-02-05 11:02:43 +0100

    fin du tp 3

--- TP.02/obj/Debug/net6.0/TP.02.AssemblyInfo.cs
+++ TP.02/obj/Debug/net6.0/TP.02.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TP.02")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c1a19ba8a48d931a4045274aeb4e83d35c892e05")]
 [assembly: System.Reflection.AssemblyProductAttribute("TP.02")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TP.02")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- TP.03/Time.cs
+++ TP.03/Time.cs
@@ -1,6 +1,37 @@
 namespace TP._03;
 
-public class Time
+/// <summary>
+/// Represents a time
+/// </summary>
+class Time
 {
+    private int _seconds;
+
+    #region constructeur
+
+    /// <summary>
+    /// Init the time with a number of seconds
+    /// </summary>
+    /// <param name="s">a nbr of seconds</param>
+    public Time(int s) => _seconds = s;
+
+    /// <summary>
+    /// Init the time
+    /// </summary>
+    /// <param name="h">number of hours</param>
+    /// <param name="m">number of minutes</param>
+    /// <param name="s">number of seconds</param>
+    public Time(int h, int m, int s) => _seconds = s + m*60 + h*3600;
     
+    #endregion
+    public override string ToString() => $"{Hours:D2}:{Minutes:D2}:{Seconds:D2}";
+    public int Hours => _seconds / 3600; 
+    public int Minutes => (_seconds - Hours*3600) / 60; 
+    public int Seconds => Math.Abs(_seconds - (_seconds - Hours*3600) - (_seconds - Minutes*60)); 
+    
+    /// <summary>
+    /// Add some seconds
+    /// </summary>
+    /// <param name="nbsecs">the number of seconds to add</param>
+    public void Add(int nbsecs) => _seconds += nbsecs;
 }
