616da9399c7c03b27a67607c29d3f3449bdb8ad6
Malo Mouorn <malo.mouron@gmail.com>
2024-03-04 10:04:05 +0100

    vp + comment

--- TP.06/TP.06 exo1/Account.cs
+++ TP.06/TP.06 exo1/Account.cs
@@ -2,31 +2,38 @@ using System;
 
 namespace TP._06_exercice_1
 {
-	
-public class Account {
-	
-	private string _number;
-	private int _balance;
-	private Person _client;
-	
-	public Account(Person client, string number)
-	{
-		_number = number;
-		_balance = 0;
-		_client = client;
-	}
+    public class Account
+    {
+        #region Fields
+        private string _number; //Numéro de compte
+        private int _balance;  //Solde du compte
+        private Person _client; //Titulaire du compte
+        #endregion
 
-	public void Deposit(int sum) {
-		_balance += sum;
-	}
-	public void Withdraw(int amount)
-	{
-		_balance -= amount;
-	}
-	public override string ToString() {
-		return "Account number "+_number+" – "+_client.ToString()+" - Balance : "+_balance+" euros";
-	}
+        #region Constructor
+        public Account(Person client, string number) //Constructeur de la classe Account qui prend en paramètre le titulaire du compte et le numéro de compte
+        {
+            _number = number;
+            _balance = 0;
+            _client = client;
+        }
+        #endregion
 
+        #region Methods
+        public void Deposit(int sum) //Méthode pour déposer de l'argent
+        {
+            _balance += sum;
+        }
 
-}
+        public void Withdraw(int amount) //Méthode pour retirer de l'argent
+        {
+            _balance -= amount;
+        }
+
+        public override string ToString() //Méthode pour retourner les informations du compte sous forme de chaîne de caractères
+        {
+            return "Account number "+_number+" – "+_client.ToString()+" - Balance : "+_balance+" euros";
+        }
+        #endregion
+    }
 }
--- TP.06/TP.06 exo1/Person.cs
+++ TP.06/TP.06 exo1/Person.cs
@@ -2,20 +2,26 @@ using System;
 
 namespace TP._06_exercice_1
 {
-	public class Person
-	{
-		private string _lastName;
-		private string _firstName;
-		
-		public Person(string lastName, string firstName)
-		{
-			_lastName = lastName;
-			_firstName = firstName;
-		}
+    public class Person
+    {
+        #region Fields
+        private string _lastName; //Nom de la personne
+        private string _firstName; //Prénom de la personne
+        #endregion
 
-		public override string ToString()
-		{
-			return string.Concat(_firstName, " ", _lastName);
-		}
-	}
+        #region Constructor
+        public Person(string lastName, string firstName) //Constructeur de la classe Person qui prend en paramètre le nom et le prénom de la personne
+        {
+            _lastName = lastName;
+            _firstName = firstName;
+        }
+        #endregion
+
+        #region Methods
+        public override string ToString() //Méthode pour afficher les informations du compte sous forme de chaîne de caractères
+        {
+            return string.Concat(_firstName, " ", _lastName);
+        }
+        #endregion
+    }
 }
