850162ca9d3c36121585f1d30be67013dd9fe73f
mathis-de-sousa-pro <157818653+mathis-de-sousa-pro@users.noreply.github.com>
2024-03-22 08:29:08 +0100

    rajout deux changement dans la partie xaml

--- TP.10/TP.10/MainWindow.xaml.cs
+++ TP.10/TP.10/MainWindow.xaml.cs
@@ -1,61 +1,47 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 using TP;
 using TP10.Logic;
 
-namespace TP10
+namespace TP10;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+    private Zoo _zoo;
+
+    public MainWindow()
     {
-        private Zoo zoo;
+        InitializeComponent();
+        _zoo = new Zoo();
+        var list = _zoo.ListSpecies();
+        foreach (var s in list)
+            species.Items.Add(s);
+        ListAnimals();
+    }
 
-        public MainWindow()
+    private void ListAnimals()
+    {
+        animals.Items.Clear();
+        foreach (Animal a in _zoo.ListAnimals())
         {
-            InitializeComponent();
-            zoo = new Zoo();
-            var list = zoo.ListSpecies();
-            foreach (string s in list)
-                species.Items.Add(s);
+            animals.Items.Add(a);
+        }
+    }
+
+    private void AddAnimal(object sender, RoutedEventArgs e)
+    {
+        _zoo.AddAnimal(name.Text, species.SelectedItem.ToString());
+        ListAnimals();
+    }
+
+    private void RemoveAnimal(object sender, RoutedEventArgs e)
+    {
+        if (animals.SelectedItem is Animal a)
+        {
+            _zoo.RemoveAnimal(a);
             ListAnimals();
-        }
-
-        private void ListAnimals()
-        {
-            animals.Items.Clear();
-            foreach(Animal a in zoo.ListAnimals())
-            {
-                animals.Items.Add(a);
-            }
-        }
-
-        private void AddAnimal(object sender, RoutedEventArgs e)
-        {
-            zoo.AddAnimal(name.Text, species.SelectedItem.ToString());
-            ListAnimals();
-        }
-
-        private void RemoveAnimal(object sender, RoutedEventArgs e)
-        {
-            if(animals.SelectedItem is Animal a)
-            {
-                zoo.RemoveAnimal(a);
-                ListAnimals();
-            }
         }
     }
 }
