d23643e2e7aef1bae9c9f3a205513c24957bd32e
mathis-de-sousa-pro <157818653+mathis-de-sousa-pro@users.noreply.github.com>
2024-03-22 08:33:43 +0100

    oui

--- TP.10/TP.10/Logic/AnimalFactory.cs
+++ TP.10/TP.10/Logic/AnimalFactory.cs
@@ -1,40 +1,49 @@
-﻿namespace TP
+﻿using TP;
+
+namespace TP10.Logic;
+
+/// <summary>
+/// Fabrique de la classe Animal
+/// </summary>
+public class AnimalFactory
 {
     /// <summary>
-    /// Fabrique de la classe Animal
+    /// Creer un nouvel Animal
     /// </summary>
-    public class AnimalFactory
+    /// <param name="name">Nom de l'animal</param>
+    /// <param name="type">Espece de l'animal</param>
+    /// <returns>L'animal créé, ou null si le type donné est inconnu</returns>
+    public Animal CreateAnimal(string name, string type)
     {
-        /// <summary>
-        /// Creer un nouvel Animal
-        /// </summary>
-        /// <param name="name">Nom de l'animal</param>
-        /// <param name="type">Espece de l'animal</param>
-        /// <returns>L'animal créé, ou null si le type donné est inconnu</returns>
-        public Animal CreateAnimal(string name, string type)
+        Animal animal = null;
+        switch (type.ToLower())
         {
-            Animal animal = null;
-            switch (type.ToLower())
-            {
-                case "human": animal = new Human(name); break;
-                case "eagle": animal = new Eagle(name); break;
-                case "vulture": animal = new Vulture(name); break;
-                case "cocker": animal = new Cocker(name); break;
-                case "sheperd": animal = new Sheperd(name); break;
-                case "cat": animal = new Cat(name); break;
-            }
-
-            return animal;
+            case "human":
+                animal = new Human(name);
+                break;
+            case "eagle":
+                animal = new Eagle(name);
+                break;
+            case "vulture":
+                animal = new Vulture(name);
+                break;
+            case "cocker":
+                animal = new Cocker(name);
+                break;
+            case "sheperd":
+                animal = new Sheperd(name);
+                break;
+            case "cat":
+                animal = new Cat(name);
+                break;
         }
 
-        /// <summary>
-        /// Donne la liste des especes possibles
-        /// </summary>
-        /// <returns>Array de string</returns>
-        public string[] ListSpecies()
-        {
-            return new string[] { "cocker", "eagle", "human", "sheperd", "vulture", "cat" };
-        }
-
+        return animal;
     }
+
+    /// <summary>
+    /// Donne la liste des especes possibles
+    /// </summary>
+    /// <returns>Array de string</returns>
+    public string[] ListSpecies() => new string[] { "cocker", "eagle", "human", "sheperd", "vulture", "cat" };
 }
